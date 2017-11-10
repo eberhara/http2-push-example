7c6d8b56fa8d5d31c25b103c00feb3c10b2b8d11
André Eberhardt <santos.eberhardt@gmail.com>
2017-11-09 23:37:45 -0200

    Update example

--- src/server.js
+++ src/server.js
@@ -1,30 +1,39 @@
 'use strict'
 
 const fs = require('fs')
-const path = require('path')
-// eslint-disable-next-line
 const http2 = require('http2')
-const helper = require('./helper')
+const path = require('path')
+const mime = require('mime')
 
 const { HTTP2_HEADER_PATH } = http2.constants
-const PORT = process.env.PORT || 3000
-const PUBLIC_PATH = path.join(__dirname, '../public')
 
-const publicFiles = helper.getFiles(PUBLIC_PATH)
-const server = http2.createSecureServer({
-  cert: fs.readFileSync(path.join(__dirname, '../ssl/cert.pem')),
-  key: fs.readFileSync(path.join(__dirname, '../ssl/key.pem'))
-}, onRequest)
+const key = fs.readFileSync(path.resolve(__dirname, '../ssl/key.pem'))
+const cert = fs.readFileSync(path.resolve(__dirname, '../ssl/cert.pem'))
 
-function wait (ms) {
-  const start = new Date().getTime()
-  let end = start
-  while (end < start + ms) {
-    end = new Date().getTime()
-  }
+const getFiles = (baseDir) => {
+  const files = new Map()
+
+  fs.readdirSync(baseDir).forEach((fileName) => {
+    const filePath = path.join(baseDir, fileName)
+    const fileDescriptor = fs.openSync(filePath, 'r')
+    const stat = fs.fstatSync(fileDescriptor)
+    const contentType = mime.getType(filePath)
+
+    files.set(`/${fileName}`, {
+      fileDescriptor,
+      headers: {
+        'content-length': stat.size,
+        'last-modified': stat.mtime.toUTCString(),
+        'content-type': contentType
+      }
+    })
+  })
+
+  return files
 }
 
-// Push file
+const publicFiles = getFiles(path.resolve(__dirname, '../public/'))
+
 function push (stream, path) {
   const file = publicFiles.get(path)
 
@@ -37,35 +46,38 @@ function push (stream, path) {
   })
 }
 
-// Request handler
-function onRequest (req, res) {
-  wait(3000)
-  const reqPath = req.path === '/' ? '/index.html' : req.path
+const wait = (ms) => {
+  const start = new Date().getTime()
+  let end = start
+  while (end < start + ms) {
+    end = new Date().getTime()
+  }
+}
+
+const handler = (req, res) => {
+  wait(2000)
+  const path = req.headers[HTTP2_HEADER_PATH]
+  const reqPath = path === '/' ? '/index.html' : path
   const file = publicFiles.get(reqPath)
 
-  // File not found
   if (!file) {
     res.statusCode = 404
     res.end()
     return
   }
 
-  // Push with index.html
   if (reqPath === '/index.html') {
     push(res.stream, '/bundle1.js')
     push(res.stream, '/bundle2.js')
     push(res.stream, '/bundle4.js')
   }
 
-  // Serve file
   res.stream.respondWithFD(file.fileDescriptor, file.headers)
 }
 
-server.listen(PORT, (err) => {
-  if (err) {
-    console.error(err)
-    return
-  }
+const server = http2.createSecureServer({ cert, key }, handler)
 
-  console.log(`Server listening on ${PORT}`)
+server.listen(3003, () => {
+  const { address, port } = server.address()
+  console.log(`HTTP/2 with server-push server listening at ${address}:${port}`)
 })
